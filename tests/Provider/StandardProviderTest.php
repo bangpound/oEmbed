6e5e1c452fe375dfeb6f68ab2aed01ab3ccf49a5
Benjamin Doherty <bjd@bangpound.org>
2015-07-03 08:53:19 -0500

    more tests.

--- tests/Provider/StandardProviderTest.php
+++ tests/Provider/StandardProviderTest.php
@@ -1,21 +1,36 @@
 <?php
 
-namespace Provider;
-
-use Bangpound\oEmbed\Provider\StandardProvider;
+namespace Bangpound\oEmbed\Provider;
 
 class StandardProviderTest extends \PHPUnit_Framework_TestCase
 {
-    public function testSupport()
+    /**
+     * @var ProviderInterface
+     */
+    private $provider;
+
+    public function setUp()
     {
-        $provider = new StandardProvider('', array(
+        $this->provider = new StandardProvider('http://example.com/oembed', array(
           'http://*.example.com/*',
           'https://example.com/video/*',
         ));
+    }
 
-        $this->assertFalse($provider->supports('http://example.com/video'));
-        $this->assertTrue($provider->supports('http://video.example.com/something'));
-        $this->assertFalse($provider->supports('http://example.com/video/something'));
-        $this->assertTrue($provider->supports('https://example.com/video/something'));
+    public function testSupport()
+    {
+        $this->assertFalse($this->provider->supports('http://example.com/video'));
+        $this->assertTrue($this->provider->supports('http://video.example.com/something'));
+        $this->assertFalse($this->provider->supports('http://example.com/video/something'));
+        $this->assertTrue($this->provider->supports('https://example.com/video/something'));
+    }
+
+    public function testRequest()
+    {
+        $request = $this->provider->request('http://video.example.com/something');
+        $this->assertInstanceOf('Psr\\Http\\Message\\RequestInterface', $request);
+
+        $request = $this->provider->request('https://example.com/video/something');
+        $this->assertInstanceOf('Psr\\Http\\Message\\RequestInterface', $request);
     }
 }
