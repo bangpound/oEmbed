4440644372ad11dff242bb6204dd50c5ca411d62
Benjamin Doherty <bjd@bangpound.org>
2015-07-03 18:43:03 -0500

    test the serializer.

--- tests/Serializer/SerializerTest.php
+++ tests/Serializer/SerializerTest.php
@@ -2,6 +2,7 @@
 
 namespace Bangpound\oEmbed\Test\Serializer;
 
+use Bangpound\oEmbed\Response\LinkResponse;
 use Bangpound\oEmbed\Response\Response;
 use Bangpound\oEmbed\Serializer\Serializer;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
@@ -31,13 +32,19 @@ class SerializerTest extends \PHPUnit_Framework_TestCase
         $serializer = new Serializer($serializer);
         $result = $serializer->serialize($data, $format);
         $this->assertEquals($expected, $result);
+
+        $class = get_class($data);
+        $result = $serializer->deserialize($result, $class, $format);
+        $this->assertEquals($data, $result);
     }
 
     public function provideSerializer()
     {
         return array(
-            [new Response(), 'json', '{"type":null,"version":"1.0","title":null,"author_name":null,"author_url":null,"provider_name":null,"provider_url":null,"cache_age":null,"thumbnail_url":null,"thumbnail_width":null,"thumbnail_height":null}'],
+          [new Response(), 'json', '{"type":null,"version":"1.0","title":null,"author_name":null,"author_url":null,"provider_name":null,"provider_url":null,"cache_age":null,"thumbnail_url":null,"thumbnail_width":null,"thumbnail_height":null}'],
           [new Response(), 'xml', '<?xml version="1.0"?>'.PHP_EOL.'<response><type/><version>1.0</version><title/><author_name/><author_url/><provider_name/><provider_url/><cache_age/><thumbnail_url/><thumbnail_width/><thumbnail_height/></response>'.PHP_EOL],
+          [new LinkResponse(), 'json', '{"type":null,"version":"1.0","title":null,"author_name":null,"author_url":null,"provider_name":null,"provider_url":null,"cache_age":null,"thumbnail_url":null,"thumbnail_width":null,"thumbnail_height":null}'],
+          [new LinkResponse(), 'xml', '<?xml version="1.0"?>'.PHP_EOL.'<response><type/><version>1.0</version><title/><author_name/><author_url/><provider_name/><provider_url/><cache_age/><thumbnail_url/><thumbnail_width/><thumbnail_height/></response>'.PHP_EOL],
         );
     }
 }
