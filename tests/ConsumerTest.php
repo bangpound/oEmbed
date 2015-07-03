849fd23f46d2a05ffc6cdaa265701e7c10f0f380
Benjamin Doherty <bjd@bangpound.org>
2015-07-03 01:41:09 -0500

    improve test to use mock handler.

--- tests/ConsumerTest.php
+++ tests/ConsumerTest.php
@@ -5,6 +5,9 @@ namespace Bangpound\oEmbed;
 use Bangpound\oEmbed\Provider\StandardProvider;
 use Bangpound\oEmbed\Serializer\Serializer;
 use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
 use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
@@ -12,15 +15,40 @@ use Symfony\Component\Serializer\Normalizer\PropertyNormalizer;
 
 class ConsumerTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var Consumer
+     */
+    private $consumer;
+
+    public function setUp()
+    {
+        // Create a mock and queue two responses.
+        $mock = new MockHandler([
+          new Response(200, [
+              'Content-Type' => 'application/json',
+          ], '{"author_url":"http:\/\/www.youtube.com\/user\/simonyapp","thumbnail_width":480,"author_name":"Simon Yapp","height":344,"title":"TEST VIDEO","width":459,"version":"1.0","provider_name":"YouTube","html":"\u003ciframe width=\"459\" height=\"344\" src=\"https:\/\/www.youtube.com\/embed\/C0DPdy98e4c?feature=oembed\" frameborder=\"0\" allowfullscreen\u003e\u003c\/iframe\u003e","provider_url":"http:\/\/www.youtube.com\/","thumbnail_url":"https:\/\/i.ytimg.com\/vi\/C0DPdy98e4c\/hqdefault.jpg","type":"video","thumbnail_height": 360}'),
+        ]);
+
+        $handler = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handler]);
+
+        $nameConverter = new CamelCaseToSnakeCaseNameConverter();
+        $serializer = new \Symfony\Component\Serializer\Serializer([
+          new PropertyNormalizer(null, $nameConverter),
+          new GetSetMethodNormalizer(null, $nameConverter),
+        ], [
+          new JsonEncoder(),
+        ]);
+        $serializer = new Serializer($serializer);
+
+        $this->consumer = new Consumer($client, $serializer);
+    }
+
     public function testGet()
     {
-        $provider = new StandardProvider('http://www.youtube.com/oembed');
-        $client = new Client();
-        $serializer = new \Symfony\Component\Serializer\Serializer([new PropertyNormalizer(null, new CamelCaseToSnakeCaseNameConverter()), new GetSetMethodNormalizer(null, new CamelCaseToSnakeCaseNameConverter())], [new JsonEncoder()]);
-        $serializer = new Serializer($serializer);
-        $consumer = new Consumer($client, $serializer);
+        $provider = new StandardProvider('');
         /** @var \Bangpound\oEmbed\Response\VideoResponse $response */
-        $response = $consumer->get($provider, 'https://www.youtube.com/watch?v=0fOHh5Q7Q1E');
+        $response = $this->consumer->get($provider, 'https://www.youtube.com/watch?v=C0DPdy98e4c');
 
         $this->assertInstanceOf('Bangpound\\oEmbed\\Response\\Response', $response);
         $this->assertInternalType(\PHPUnit_Framework_Constraint_IsType::TYPE_INT, $response->getThumbnailHeight());
