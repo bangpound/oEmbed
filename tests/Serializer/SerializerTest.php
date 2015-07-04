3ef0af1e4f7e10678f46da6efb7ee92407f80b37
Benjamin Doherty <bjd@bangpound.org>
2015-07-03 22:32:37 -0500

    code coverage for serializer with map.

--- tests/Serializer/SerializerTest.php
+++ tests/Serializer/SerializerTest.php
@@ -16,7 +16,7 @@ class SerializerTest extends \PHPUnit_Framework_TestCase
     /**
      * @dataProvider provideSerializer
      *
-     * @param \Bangpound\oEmbed\Response\Response $data
+     * @param Response $data
      * @param $format
      */
     public function testSerialize(Response $data, $format, $expected)
@@ -38,6 +38,33 @@ class SerializerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($data, $result);
     }
 
+    /**
+     * @dataProvider provideSerializerWithMap
+     *
+     * @param Response $data
+     * @param $format
+     * @param array $map
+     * @param $expected
+     */
+    public function testSerializeWithMap(Response $data, $format, array $map = array(), $expected)
+    {
+        $nameConverter = new CamelCaseToSnakeCaseNameConverter();
+        $serializer = new \Symfony\Component\Serializer\Serializer([
+          new PropertyNormalizer(null, $nameConverter),
+          new GetSetMethodNormalizer(null, $nameConverter),
+        ], [
+          new JsonEncoder(),
+          new XmlEncoder(),
+        ]);
+        $serializer = new Serializer($serializer, $map);
+        $result = $serializer->serialize($data, $format);
+        $this->assertEquals($expected, $result);
+
+        $class = get_class($data);
+        $result = $serializer->deserialize($result, $class, $format);
+        $this->assertInstanceOf($class, $result);
+    }
+
     public function provideSerializer()
     {
         return array(
@@ -45,6 +72,16 @@ class SerializerTest extends \PHPUnit_Framework_TestCase
           [new Response(), 'xml', '<?xml version="1.0"?>'.PHP_EOL.'<response><type/><version>1.0</version><title/><author_name/><author_url/><provider_name/><provider_url/><cache_age/><thumbnail_url/><thumbnail_width/><thumbnail_height/></response>'.PHP_EOL],
           [new LinkResponse(), 'json', '{"type":null,"version":"1.0","title":null,"author_name":null,"author_url":null,"provider_name":null,"provider_url":null,"cache_age":null,"thumbnail_url":null,"thumbnail_width":null,"thumbnail_height":null}'],
           [new LinkResponse(), 'xml', '<?xml version="1.0"?>'.PHP_EOL.'<response><type/><version>1.0</version><title/><author_name/><author_url/><provider_name/><provider_url/><cache_age/><thumbnail_url/><thumbnail_width/><thumbnail_height/></response>'.PHP_EOL],
+        );
+    }
+
+    public function provideSerializerWithMap()
+    {
+        return array(
+          [new Response(), 'json', [], '{"type":null,"version":"1.0","title":null,"author_name":null,"author_url":null,"provider_name":null,"provider_url":null,"cache_age":null,"thumbnail_url":null,"thumbnail_width":null,"thumbnail_height":null}'],
+          [new Response(), 'xml', [], '<?xml version="1.0"?>'.PHP_EOL.'<response><type/><version>1.0</version><title/><author_name/><author_url/><provider_name/><provider_url/><cache_age/><thumbnail_url/><thumbnail_width/><thumbnail_height/></response>'.PHP_EOL],
+          [new LinkResponse(), 'json', [], '{"type":null,"version":"1.0","title":null,"author_name":null,"author_url":null,"provider_name":null,"provider_url":null,"cache_age":null,"thumbnail_url":null,"thumbnail_width":null,"thumbnail_height":null}'],
+          [new LinkResponse(), 'xml', [], '<?xml version="1.0"?>'.PHP_EOL.'<response><type/><version>1.0</version><title/><author_name/><author_url/><provider_name/><provider_url/><cache_age/><thumbnail_url/><thumbnail_width/><thumbnail_height/></response>'.PHP_EOL],
         );
     }
 }
